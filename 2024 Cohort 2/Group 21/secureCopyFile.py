0f7ff857fbf0bc8f3c87a294662bb96a92472b6e
sprater <sprater@gmailcom>
2024-11-20 08:50:07 -0600

    Make file check work on folders, too

--- 2024 Cohort 2/Group 21/secureCopyFile.py
+++ 2024 Cohort 2/Group 21/secureCopyFile.py
@@ -2,9 +2,9 @@
 
 import os.path
 
-def check_file_exists(file_path: str) -> bool:
-    print('\n Checking for existence of "' + file_path + "'")
-    return os.path.isfile(file_path)
+def check_path_exists(path: str) -> bool:
+    print('\n Checking for existence of "' + path + "'")
+    return os.path.exists(path)
 
 def check_file_has_data(file_path: str) -> bool:
     print('\n Checking file length')
@@ -15,14 +15,16 @@ def check_file_has_data(file_path: str) -> bool:
         return False
 
 # Change these to test in your environment
-name_of_file_to_be_copied: str = 'myfile.txt'
-source_path: str = '/path/to/source/file/'
+#name_of_file_to_be_copied: str = 'myfile.txt'
+name_of_file_to_be_copied: str = 'secureCopyFile.py'
+#source_path: str = '/path/to/source/file/'
+source_path: str = './'
 destination_path: str = '/my/destination/folder/'
 
 full_source_path = source_path + name_of_file_to_be_copied
 
 # Step 1: check if the file exists
-if check_file_exists(full_source_path):
+if check_path_exists(full_source_path):
     print('Yes, the file exists\n')
 else:
     print('The file does not exist. Exiting.\n')
